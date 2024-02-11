07202a349ce00505bca43c98d2bc70bfe218da0d
Ariana <ajadrejci@unipu.hr>
2024-02-11 17:16:47 +0100

    add store

---
+++ src/store.js
@@ -0,0 +1,33 @@
+let data = {
+    wedding_details: {
+        data: {
+            id: 1,
+            mr_name: "John Johnson",
+            mrs_name: "Jane Janette",
+            date: "10/06/2024",
+            time: "14:00",
+            location: "St. Joseph Church, Washington DC",
+        },    
+   },
+   expenses: {
+        data: [
+            {
+                id: 1,
+                title: "Wedding dress",
+                price: 623.24,
+                is_paid: true,
+            },
+        ],
+   },
+   guests: {
+        data: [
+            {
+                id: 1,
+                name: "Mark Markus",
+                is_confirmed: true,
+            },
+        ],
+   },
+};
+
+export default data;
