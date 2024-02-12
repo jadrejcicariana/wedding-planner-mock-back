802632ba1e973799134c874f05410fc29faf5776
Ariana <ajadrejci@unipu.hr>
2024-02-12 09:57:19 +0100

    add patch and delete

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.get("/wedding_details/:user_id", (req, res) => {
     res.json(data.wedding_details_one);
 });
 
+//add new wedding details
 app.post("/wedding_details", (req, res) => {
     res.statusCode = 201;
     res.setHeader("Location", "/wedding_details/123");
@@ -34,22 +35,35 @@ app.post("/expenses/:user_id", (req, res) => {
 });
 
 //edit expense
-app.put("/expenses/:user_id/:id", (req, res) => {
-    
+app.put("/expenses/:user_id/:id", (req, res) => {  
     res.statusCode = 201;
     res.setHeader("Location", "/expenses/123/1");
     res.send();
 });
 
+//mark expense paid
+app.patch("/expenses/:user_id/:id", (req, res) => {
+    console.log("data", req.params.user_id, req.params.id, req.body);
+    res.statusCode = 200;
+    res.send();
+})
+
+//delete expense
+app.delete("/expenses/:user_id/:id", (req, res) => {
+    console.log("data", req.params.user_id, req.params.id);
+    res.statusCode = 200;
+    res.send();
+})
 
 //guests
 app.get("/guests/:user_id", (req, res) => {
     res.json(data.guests);
 });
 
+//add new guest
 app.post("/guests", (req, res) => {
     res.statusCode = 201;
-    res.setHeader("Location", "/guests/123");
+    res.setHeader("Location", "/guests/123/1");
     res.send();
 });
 
